2382b9f1d5beb5f6720082c52c24a1f40c53d248
Nathan Fritter <nathan.fritter@gmail.com>
2024-02-16 12:03:57 -0800

    Separating Snowflake cocktails sql into two files

--- dags/sql/write_cocktails_to_snowflake.sql
+++ dags/sql/write_cocktails_to_snowflake.sql
@@ -1,16 +1,5 @@
 use schema SANDBOX_DATA_PIPELINE.public;
 
-create or replace table cocktails_stage
-(
-    json_data variant
-);
-
-truncate cocktails_stage;
-
-COPY INTO cocktails_stage
-    FROM @sandbox_data_pipeline/cocktails/{{ task_instance.xcom_pull(task_ids='get_run_hr') }}/
-FILE_FORMAT = (TYPE = JSON);
-
 create table if not exists cocktails
 (
     run_hr      int,
