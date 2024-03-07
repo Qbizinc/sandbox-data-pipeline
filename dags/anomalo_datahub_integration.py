b005f679e100a79b76f474c1ec84bde7ad8733b2
Nathan Fritter <nathan.fritter@gmail.com>
2024-03-07 15:55:47 -0800

    Moving Anomalo credentials pull and API client creation into Airflow tasks

--- dags/anomalo_datahub_integration.py
+++ dags/anomalo_datahub_integration.py
@@ -22,19 +22,20 @@ bigquery_weather_prod_table = 'qbiz-bigquery-sandbox-pipeline.sandbox_data_pipel
 snowflake_weather_prod_table = 'qbiz-snowflake-sandbox-pipeline.public.weather'
 #TODO: Add Snowflake cocktails table once Anomalo <> Snowflake issues are figured out
 
-# Get Anomalo API credentials from AWS parameters
-anomalo_instance_host = get_aws_parameter("sandbox_data_pipeline__anomalo_api_host")
-anomalo_api_secret_token = get_aws_parameter("sandbox_data_pipeline__anomalo_api_token")
+@task
+def get_anomalo_creds():
+    # Get Anomalo API credentials from AWS parameter store, return as XCOMs
+    anomalo_instance_host = get_aws_parameter("sandbox_data_pipeline__anomalo_api_host")
+    anomalo_api_secret_token = get_aws_parameter("sandbox_data_pipeline__anomalo_api_token")
 
-# Create client object that the rest of the DAG can reuse
-anomalo_client = anomalo.Client(host=anomalo_instance_host, api_token=anomalo_api_secret_token)
+    return anomalo_instance_host, anomalo_api_secret_token
+
 
 @task.sensor(poke_interval=30, timeout=3600, mode="poke")
-def anomalo_check_sensor(api_client: anomalo.Client, table_name: str, start_date: Optional[str] = None, end_date: Optional[str] = None, **kwargs) -> PokeReturnValue:
+def anomalo_check_sensor(table_name: str, start_date: Optional[str] = None, end_date: Optional[str] = None, **kwargs) -> PokeReturnValue:
     """
     Check if Anomalo checks for a given table are completed via Anomalo API
     Args:
-        api_client: Authenticated Anomalo API Client (will not work if no client is passed)
         table_name: Table name (as seen in Anomalo) to check for a completed Anomalo run
         start_date: Optional start date of Anomalo check run interval to be inserted into Datahub (in "%Y-%m-%d" format)
         end_date: Optional end date of Anomalo check run interval to be inserted into Datahub (in "%Y-%m-%d" format)
@@ -43,8 +44,15 @@ def anomalo_check_sensor(api_client: anomalo.Client, table_name: str, start_date
         PokeReturnValue: Boolean dictating whether the sensor keeps checking Anomalo check results or ends and is marked as successful
     """
 
+    # Grab credentials from above task and create Anomalo client
+    ti = kwargs["ti"]
+    anomalo_creds = ti.xcom_pull(task_ids="get_anomalo_creds")
+    anomalo_instance_host = anomalo_creds[0]
+    anomalo_api_secret_token = anomalo_creds[1]
+    anomalo_client = anomalo.Client(host=anomalo_instance_host, api_token=anomalo_api_secret_token)
+
     # Use Anomalo API client to get table ID from table name
-    table_id = api_client.get_table_information(table_name=table_name)["id"]
+    table_id = anomalo_client.get_table_information(table_name=table_name)["id"]
 
     # Default Behavior (if no dates supplied): Fill in start date (yesterday's date) and end date (today's date)
     if start_date is None and end_date is None:
@@ -74,22 +82,29 @@ def anomalo_check_sensor(api_client: anomalo.Client, table_name: str, start_date
 
 
 @task
-def write_anomalo_data_to_datahub(api_client: anomalo.Client, start_date: Optional[str] = None, end_date: Optional[str] = None, **kwargs):
+def write_anomalo_data_to_datahub(start_date: Optional[str] = None, end_date: Optional[str] = None, **kwargs):
     '''
     Simple task wrapper function to run below function imported from anomalo_datahub.py
     Args:
-        api_client: Authenticated Anomalo API Client (will not work if no client is passed)
         start_date: Optional start date of Anomalo check interval to be inserted into Datahub (in "%Y-%m-%d" format)
         end_date: Optional end date of Anomalo check interval to be inserted into Datahub (in "%Y-%m-%d" format)
     '''
-
+    
     # Moving "expensive" import inside task to reduce DAG parsing time
     from include.utils.anomalo_datahub import anomalo_to_datahub
+
+    # Grab credentials from above task and create Anomalo client
+    ti = kwargs["ti"]
+    anomalo_creds = ti.xcom_pull(task_ids="get_anomalo_creds")
+    anomalo_instance_host = anomalo_creds[0]
+    anomalo_api_secret_token = anomalo_creds[1]
+    anomalo_client = anomalo.Client(host=anomalo_instance_host, api_token=anomalo_api_secret_token)
+
     # Default Behavior (if no dates supplied): Fill in start date (yesterday's date) and end date (today's date)
     if start_date is None and end_date is None:
         start_date = kwargs["ds"]
         end_date = ds_add(kwargs["ds"], 1)
-    anomalo_to_datahub(api_client=api_client, start_date=start_date, end_date=end_date)
+    anomalo_to_datahub(api_client=anomalo_client, start_date=start_date, end_date=end_date)
 
 @dag(
     default_args={
@@ -110,13 +125,17 @@ def anomalo_datahub_integration():
     start_task = EmptyOperator(task_id="start")
     finish_task = EmptyOperator(task_id="finish", trigger_rule="none_failed")
 
-    bigquery_cocktails_anomalo_check_sensor_task = anomalo_check_sensor.override(task_id='bigquery_cocktails_anomalo_check_sensor')(api_client=anomalo_client, table_name=bigquery_cocktails_prod_table)
-    bigquery_weather_anomalo_check_sensor_task = anomalo_check_sensor.override(task_id='bigquery_weather_anomalo_check_sensor')(api_client=anomalo_client, table_name=bigquery_cocktails_prod_table)
-    snowflake_weather_anomalo_checks_sensor_task = anomalo_check_sensor.override(task_id='snowflake_weather_anomalo_checks_sensor')(api_client=anomalo_client, table_name=bigquery_cocktails_prod_table)
-    
-    write_anomalo_data_to_datahub_task = write_anomalo_data_to_datahub(api_client=anomalo_client)
+    get_anomalo_creds_task = get_anomalo_creds()
 
-    start_task >> [bigquery_cocktails_anomalo_check_sensor_task, bigquery_weather_anomalo_check_sensor_task, snowflake_weather_anomalo_checks_sensor_task] >> write_anomalo_data_to_datahub_task
+    bigquery_cocktails_anomalo_check_sensor_task = anomalo_check_sensor.override(task_id='bigquery_cocktails_anomalo_check_sensor')(table_name=bigquery_cocktails_prod_table)
+    bigquery_weather_anomalo_check_sensor_task = anomalo_check_sensor.override(task_id='bigquery_weather_anomalo_check_sensor')(table_name=bigquery_cocktails_prod_table)
+    snowflake_weather_anomalo_checks_sensor_task = anomalo_check_sensor.override(task_id='snowflake_weather_anomalo_checks_sensor')(table_name=bigquery_cocktails_prod_table)
+    
+    write_anomalo_data_to_datahub_task = write_anomalo_data_to_datahub()
+
+    start_task >> get_anomalo_creds_task >> [bigquery_cocktails_anomalo_check_sensor_task, 
+                                             bigquery_weather_anomalo_check_sensor_task, 
+                                             snowflake_weather_anomalo_checks_sensor_task] >> write_anomalo_data_to_datahub_task
     write_anomalo_data_to_datahub_task >> finish_task
 
 anomalo_datahub_integration()
