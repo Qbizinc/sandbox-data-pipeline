3a434a0c6aae568a7c439bb633e767780c7673b3
Nathan Fritter <nathan.fritter@gmail.com>
2024-03-26 15:14:26 -0700

    Removing other test workflows

--- .github/workflows/test_sums_integration_tests.py
+++
@@ -1,9 +0,0 @@
-def test_sum():
-    assert sum([1, 2, 3]) == 6, "Should be 6"
-
-def test_sum_tuple():
-    assert sum((10, 20, 30)) == 60, "Should be 60"
-
-def test_all_sum_unit_tests():
-    test_sum()
-    test_sum_tuple()
